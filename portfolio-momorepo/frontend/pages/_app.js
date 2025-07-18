be5e054012692517f6465cf2eca6d2d759bf3484
Shrivastava <ssyr9@umsystem.edu>
2025-07-18 14:02:06 -0500

    added next js code

---
+++ portfolio-momorepo/frontend/pages/_app.js
@@ -0,0 +1,8 @@
+import Layout from '../components/Layout';
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
