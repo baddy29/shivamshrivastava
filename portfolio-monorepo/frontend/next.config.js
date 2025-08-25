1dccc0949b5bff090e422b57b379767ea45279ac
Shivam Shrivastava <shivam@mu-593617.dhcp.missouri.edu>
2025-08-24 21:55:44 -0500

    updated api base url

--- portfolio-monorepo/frontend/next.config.js
+++ portfolio-monorepo/frontend/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
-  output: 'export'
-  // env: {
-  //   API_BASE: process.env.API_BASE
-  // }
+  output: 'export',
+  env: {
+    API_BASE: process.env.API_BASE
+  }
 }
