2237c87cd815b74319b9192b9683ccbf954cd7ff
Shivam Shrivastava <shivam@mu-593188.dhcp.missouri.edu>
2026-02-23 17:54:01 -0600

    Transition issue #27

--- portfolio-monorepo/frontend/pages/_app.js
+++ portfolio-monorepo/frontend/pages/_app.js
@@ -1,8 +1,41 @@
+import { useRouter } from 'next/router';
+import { AnimatePresence, motion } from 'framer-motion';
 import Layout from '../components/Layout';
+
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    y: 8,
+  },
+  enter: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.3, ease: 'easeOut' },
+  },
+  exit: {
+    opacity: 0,
+    y: -8,
+    transition: { duration: 0.2, ease: 'easeIn' },
+  },
+};
+
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.div
+          key={router.pathname}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+          variants={pageVariants}
+          style={{ width: '100%' }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
